2daefe816aabd16e88a3cf0dc4ee05b07031e8f5
TommyOliveir <laridatommy@gmail.com>
2023-03-01 13:13:43 +0100

    add cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
         <header>
             <Link to='/'><h2>Pic Some</h2></Link>
             <Link to='/cart'>
-                <i className={`${cartClassName} ri-fw ri-2x`}></i>
+                <i className={`${cartClassName} ri-fw ri-2x`}> {cartItems.length > 0 ?  <span class="badge">{cartItems.length}</span> : ''}</i>  
             </Link>
         </header>
     )
