b80c765c3d9f5b12b233eb352f3a3bd8e26c2172
TommyOliveir <laridatommy@gmail.com>
2023-03-01 16:00:22 +0100

    cool

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,12 @@ import { Context } from "../Context"
 function Header() {
     const { cartItems } = useContext(Context)
     const cartClassName = cartItems.length > 0 ? "ri-shopping-cart-fill" : "ri-shopping-cart-line"
+    const cartBadgeNumbers = cartItems.length
     return (
         <header>
             <Link to='/'><h2>Pic Some</h2></Link>
             <Link to='/cart'>
-                <i className={`${cartClassName} ri-fw ri-2x`}> </i>  {cartItems.length > 0 ?  <span class="badge">{cartItems.length}</span> : ''}
+                <i className={`${cartClassName} ri-fw ri-2x`}> </i>  {cartItems.length > 0 ?  <span class="badge">{cartBadgeNumbers}</span> : ''}
             </Link>
         </header>
     )
