48fc3aed0e9371ca9d72838a35c1e82744c67665
TommyOliveir <laridatommy@gmail.com>
2023-03-01 12:51:05 +0100

    add Local storage

--- src/Context.jsx
+++ src/Context.jsx
@@ -3,15 +3,27 @@ import React, { useState, useEffect } from "react"
 const Context = React.createContext()
 
 function ContextProvider({ children }) {
+  
+    const allPhotosLocalStorage = JSON.parse(localStorage.getItem("allPhotosLocalStorage"))
     const [allPhotos, setAllPhotos] = useState([])
-    const [cartItems, setCartItems] = useState([])
+    const [cartItems, setCartItems] = useState(() => JSON.parse(localStorage.getItem("cartItems")) || [])
 
     const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
     useEffect(() => {
         fetch(url)
             .then(res => res.json())
-            .then(data => setAllPhotos(data))
+            .then(data => setAllPhotos(allPhotosLocalStorage ? allPhotosLocalStorage : data))
     }, [])
+
+    useEffect(() => {
+        localStorage.setItem("cartItems", JSON.stringify(cartItems))
+    }, [cartItems])
+
+    useEffect(() => {
+        localStorage.setItem("allPhotosLocalStorage", JSON.stringify(allPhotos))
+    }, [allPhotos])
+
+    // localStorage.removeItem('allPhotosLocalStorage');
 
     function toggleFavorite(id) {
         const updatedArr = allPhotos.map(photo => {
